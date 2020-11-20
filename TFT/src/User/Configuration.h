b57309ca85cc00a49b5c3c51cce7dbe5123c1232
Mehmet Sutas <msutas@rigid3d.com>
2020-11-20 17:00:30 +0300

    701-702

--- TFT/src/User/Configuration.h
+++ TFT/src/User/Configuration.h
@@ -187,7 +187,7 @@
 
 #define HEAT_MAX_TEMP    {260,       275,       275,       275,       275,       275,       110,    60}   //max temperature can be set
 #define HEAT_SIGN_ID     {"T0:",     "T1:",     "T2:",     "T3:",     "T4:",     "T5:",     "B:",   "C:"}
-#define HEAT_DISPLAY_ID  {"T0",      "T1",      "T2",      "T3",      "T4",      "T5",      "Tab.",  "Kabin"}
+#define HEAT_DISPLAY_ID  {"T0",      "T1",      "T2",      "T3",      "T4",      "T5",      "Tbl",  "Kabin"}
 #define HEAT_CMD         {"M104 T0", "M104 T1", "M104 T2", "M104 T3", "M104 T4", "M104 T5", "M140", "M141"};
 #define HEAT_WAIT_CMD    {"M109 T0", "M109 T1", "M109 T2", "M109 T3", "M109 T4", "M109 T5", "M190", "M191"};
 
