4c930a9eb76e13e76efdd338dd9333b3952149ee
Mehmet Sutas <msutas@rigid3d.com>
2021-01-31 19:17:02 +0300

    M104/109 Düzeltme Gerekli mi? Baskı Bitince Ana Ekrana Dönülmeli mi?

--- TFT/src/User/API/Printing.c
+++ TFT/src/User/API/Printing.c
@@ -487,6 +487,13 @@ void loopCheckPrinting(void)
     }
   }
 
+/*  SUTAS???
+  if (!infoPrinting.printing && (infoMenu.menu[infoMenu.cur] == menuPrinting) && infoSettings.print_summary)
+  {
+    infoMenu.cur = 0;
+  }
+*/
+
   if (infoFile.source < BOARD_SD) return;
   if (infoMachineSettings.autoReportSDStatus == ENABLED) return;
   if (!infoSettings.m27_active && !infoPrinting.printing) return;
